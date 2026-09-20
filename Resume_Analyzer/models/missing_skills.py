f62732ee71772923fe6071eeaa952d3efc770748
deepti chauhan <deeptichauhan399@gmail.com>
2026-09-20 23:13:20 +0530

    Delete Resume_Analyzer directory

--- Resume_Analyzer/models/missing_skills.py
+++
@@ -1,8 +0,0 @@
-def get_missing_skills(resume_text, job_description):
-
-    resume_words = set(resume_text.lower().split())
-    jd_words = set(job_description.lower().split())
-
-    missing = jd_words - resume_words
-
-    return list(missing)[:10]
--- Resume_Analyzer/models/resume_summary.py
+++
@@ -1,7 +0,0 @@
-def generate_summary(skills):
-
-    return (
-        "Candidate possesses skills in "
-        + ", ".join(skills[:8])
-        + " and demonstrates strong technical capabilities."
-    )
